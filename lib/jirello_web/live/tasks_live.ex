b96a9a2adef2d3fdc70d110a3d4b4797c6b82560
Alexandru Cazacu <alexandru.cazacu.9889@gmail.com>
2022-08-06 00:32:35 +0200

    feat(Tasks): :lipstick: Converted the status field into a dropdown

--- lib/jirello_web/live/tasks_live.ex
+++ lib/jirello_web/live/tasks_live.ex
@@ -1,16 +1,44 @@
 defmodule JirelloWeb.TasksLive do
   use JirelloWeb, :live_view
 
+  alias Jirello.Accounts
   alias Jirello.Tasks
 
-  def mount(_params, _session, socket) do
+  def mount(_params, %{"user_token" => user_token} = _session, socket) do
     Tasks.subscribe()
+
+    options = %{
+      page: 1,
+      per_page: 10,
+      sort_by: :title,
+      sort_order: :asc
+    }
+
+    socket =
+      assign(socket,
+        current_user: Accounts.get_user_by_session_token(user_token),
+        options: options
+      )
 
     {:ok, fetch(socket)}
   end
 
   def handle_event("add", %{"task" => task}, socket) do
-    Tasks.create_task(task)
+    task = task |> Map.put_new("creator_id", socket.assigns.current_user.id)
+
+    case Tasks.create_task(task) do
+      {:ok, _task} ->
+        {:noreply, socket}
+
+      {:error, changeset} ->
+        socket = socket |> put_flash(:info, "Error")
+        {:noreply, assign(socket, changeset: changeset)}
+    end
+  end
+
+  def handle_event("select-status", %{"id" => id, "done" => done}, socket) do
+    task = Tasks.get_task!(id)
+    Tasks.update_task(task, %{done: done})
 
     {:noreply, socket}
   end
@@ -48,6 +76,7 @@ defmodule JirelloWeb.TasksLive do
     {:noreply, socket}
   end
 
+  # PubSub update
   def handle_info({Tasks, [:task | _], _}, socket) do
     {:noreply, fetch(socket)}
   end
@@ -78,11 +107,24 @@ defmodule JirelloWeb.TasksLive do
   end
 
   defp fetch(socket) do
-    paginate_options = %{page: 1, per_page: 5}
-    tasks = Tasks.list_tasks(paginate: paginate_options)
+    paginate_options = %{
+      page: socket.assigns.options.page,
+      per_page: socket.assigns.options.per_page
+    }
+
+    sort_options = %{
+      sort_by: socket.assigns.options.sort_by,
+      sort_order: socket.assigns.options.sort_order
+    }
+
+    tasks =
+      Tasks.list_tasks(
+        paginate: paginate_options,
+        sort: sort_options
+      )
 
     assign(socket,
-      options: paginate_options,
+      options: Map.merge(paginate_options, sort_options),
       tasks: tasks
     )
   end
