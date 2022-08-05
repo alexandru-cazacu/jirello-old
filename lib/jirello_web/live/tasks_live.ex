b95fb8098f31b3258ee0271c314a73da7abf31ae
Alexandru Cazacu <alexandru.cazacu.9889@gmail.com>
2022-08-05 22:13:29 +0200

    refactor: :lock: Remove String.to_atom/1

--- lib/jirello_web/live/tasks_live.ex
+++ lib/jirello_web/live/tasks_live.ex
@@ -56,8 +56,8 @@ defmodule JirelloWeb.TasksLive do
     page = String.to_integer(params["page"] || "1")
     per_page = String.to_integer(params["per_page"] || "5")
 
-    sort_by = (params["sort_by"] || "id") |> String.to_atom()
-    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+    sort_by = (params["sort_by"] || "id") |> String.to_existing_atom()
+    sort_order = (params["sort_order"] || "asc") |> String.to_existing_atom()
 
     paginate_options = %{page: page, per_page: per_page}
     sort_options = %{sort_by: sort_by, sort_order: sort_order}
